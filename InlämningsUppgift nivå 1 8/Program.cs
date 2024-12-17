de15b3f427b279b3075ce8b84dbc9213f50db0bb
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-17 13:50:24 +0100

    Add project files.

---
+++ InlämningsUppgift nivå 1 8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InlämningsUppgift_._nivå1_._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int produkt = 1;
+
+            for( int i = 1;i <= 5;i++ )
+            {
+                produkt *= i;
+            }
+            Console.WriteLine("Produkten av heltalen 1 till 5 är " + produkt);
+        } 
+    }
+}
